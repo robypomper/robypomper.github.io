c0768c7d65ff9505f1b0b4bca455df6fff0df3e5
Roberto Pompermaier <robypomper@gmail.com>
2023-07-06 22:00:33 +0200

    feat(HomePage): Customized HomePage for www.robypomper.org
    * Updated HomepageHeader component with a background image `static/img/home_img.png` and some styles
    * Removed HomepageFeatures component from `src/components/HomepageFeatures/`
    * Added HomepageOtherWebSites, HomepageSections and HomepageSocial functions into `src/pages/index.js`

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,26 +3,77 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner, styles.header_img)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <h1 className={clsx('hero__title', styles.header_titles)}>{siteConfig.title}</h1>
+        <p className={clsx('hero__subtitle', styles.header_titles)}>{siteConfig.tagline}</p>
       </div>
     </header>
+  );
+}
+
+function HomepageOtherWebSites() {
+  return (
+    <section className={styles.home_section}>
+      <div className="container">
+        <h1>Other RobyPomper's Web Sites</h1>
+        <div className="row">
+          <ul>
+            <li>
+              <Link to="https://www.johnosproject.org">John OS Project</Link><br/>
+              (<Link to="https://specs.johnosproject.org">Specs</Link>
+              | <Link to="https://docs.johnosproject.org">Docs</Link>
+              | <Link to="https://jcp.johnosproject.org">Public JCP</Link>)
+            </li>
+            <li><Link to="https://smartvan.johnosproject.org">Smart Van</Link></li>
+            <li><Link to="https://smartaladino.johnosproject.org">Smart Aladino</Link></li>
+            <li><Link to="https://utils_libs.robypomper.org">Utils Libs</Link></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function HomepageSections() {
+  return (
+    <section className={styles.home_section}>
+      <div className="container">
+      <div className={styles.home_section_sub_right}>
+        <h1>My Sections</h1>
+        <div className="row">
+          <ul>
+            <li><Link to="docs/intro">My Presentation</Link></li>
+            <li><Link to="docs/category/my-projects">My Projects</Link></li>
+            <li><Link to="docs/category/my-sports">My Sports</Link></li>
+            <li><Link to="docs/photos">My Photos</Link></li>
+          </ul>
+        </div>
+      </div>
+      </div>
+    </section>
+  );
+}
+
+function HomepageSocial() {
+  return (
+    <section className={styles.home_section}>
+      <div className="container">
+        <h1>RobyPomper's Social</h1>
+        <div className="row">
+          <ul>
+            <li><Link to="https://www.linkedin.com/in/roberto-pompermaier-474a4132/">Linkedin</Link></li>
+            <li><Link to="https://github.com/robypomper">GitHub</Link></li>
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 }
 
@@ -31,10 +82,12 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Roberto Pompermaier's personal web site">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HomepageOtherWebSites />
+        <HomepageSections />
+        <HomepageSocial />
       </main>
     </Layout>
   );
